50afe0e8090e1b9704955416c2ffc5a1296f284a
João Alarcón <joao.alarcon25@gmail.com>
2022-02-21 23:03:34 -0300

    Initial commit

---
+++ visualAnalytics/tests/src/main/java/joao/alarcon/tests/kabum/TestKabumApplication.java
@@ -0,0 +1,15 @@
+package joao.alarcon.tests.kabum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestKabumApplication 
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(TestKabumApplication.class, args);
+	}
+
+}
